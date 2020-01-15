10240cbae00a84e0dcf773373c7610862bf88ef4
Milo <milo.voorhout@student.hu.nl>
2020-01-15 21:48:10 +0100

    BusinersRuleType and Category class filled

--- src/main/java/tosad/define/BusinessRuleType.java
+++ src/main/java/tosad/define/BusinessRuleType.java
@@ -1,4 +1,37 @@
 package tosad.define;
 
 public class BusinessRuleType {
+    private String name;
+    private String description;
+    private int categoryID;
+
+    public BusinessRuleType(String name, String description, int categoryID) {
+        this.name = name;
+        this.description = description;
+        this.categoryID = categoryID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
 }
