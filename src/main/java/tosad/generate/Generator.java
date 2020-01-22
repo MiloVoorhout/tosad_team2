7cff4875d5c6d1400b64530745426c1b59abe0d8
Milo <milo.voorhout@student.hu.nl>
2020-01-22 13:55:17 +0100

    Generate now has diffrent rules

--- src/main/java/tosad/generate/Generator.java
+++ src/main/java/tosad/generate/Generator.java
@@ -1,12 +1,33 @@
 package tosad.generate;
 
+import tosad.attribute.Attribute;
 import tosad.define.BusinessRule;
+import tosad.define.BusinessRuleFacade;
+import tosad.generate.type.CompareRule;
+import tosad.generate.type.ListRule;
+import tosad.generate.type.RangeRule;
 
 public class Generator {
 
+    public void getAttributeData(){
 
-    public void generateCode(BusinessRule rule) {
+    }
+    public void generateCode(BusinessRule rule, Attribute attribute){
 
+        int typeID = rule.getBusinessRuleTypeID();
+        switch(typeID) {
+            case 1:
+                RangeRule.getSymbol(attribute.getName(), attribute.getTabel(), attribute.getDatabase());
+                break;
+            case 2:
+                CompareRule.getSymbol(attribute.getName(), attribute.getTabel(), attribute.getDatabase());
+                break;
+            case 3:
+                ListRule.getSymbol(attribute.getName(), attribute.getTabel(), attribute.getDatabase());
+                break;
+            default:
+                System.out.println("Ongeldig nummer");
+        }
 
 //        System.out.println("CREATE OR REPLACE TRIGGER Testnaam " +
 //                "BEFORE INSERT ON " + "Tabelnaam" + "FOR EACH ROW " +
