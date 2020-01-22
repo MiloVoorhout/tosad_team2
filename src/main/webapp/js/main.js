fd8d35e3680cde0e247fd50b76d001d163657120
Stefan <stefan.jovanovic@live.nl>
2020-01-22 22:56:33 +0100

    Added validations for define

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -11,12 +11,10 @@ const properties = $("#properties");
 const list = $("#list");
 const listValue = $('#newListItemValue');
 const listItems = $('#listValues');
-const listActionsContainer = $('#listActions');
 const tableSelect = $('#table_select')
 const attributeSelect = $('#attribute_select')
 const clearAllListBtn = $('#clearAllList')
 
-rule_type_select.val(0);
 properties.hide();
 
 var arrayObj = [compareWith, operator, operatorCompare, compare_with_select, operator_select_list, range, literalValue, entityAttribute, list, listValue, listItems];
@@ -27,6 +25,7 @@ function hideAllObj(){
 }
 
 rule_type_select.on('change', function (e) {
+    setAttributes();
     if (this.value === '1') {
         hideAllObj();
         operator.show();
@@ -60,30 +59,16 @@ compare_with_select.on('change', function (e) {
     }
 });
 
-listValue.bind("enterKey",function(e){
-    if ($(this).val() !== '') {
-        let newValue = $(this).val().replace(/ /g,'');
-        listItems.append("<option value='"+ newValue +"'>" + newValue + "</option>");
-        $(this).val("");
-    }
-});
-
-listValue.keyup(function(e){
-    if(e.keyCode == 13)
+listValue.keypress(function(event){
+    if (event.keyCode == 10 || event.keyCode == 13)
     {
+        event.preventDefault();
         $(this).trigger("enterKey");
-    }
-});
-
-listItems.on('change', function (e) {
-
-    if (listItems.val()) {
-        listActionsContainer.empty();
-        listActionsContainer.append('<button type="button" class="btn btn-danger btn-sm" id="clearAllList">Clear all</button>');
-        listActionsContainer.append('<button type="button" class="btn btn-danger btn-sm" id="clearAllList">Delete selected</button>');
-    } else {
-        listActionsContainer.empty();
-        listActionsContainer.append('<button type="button" class="btn btn-danger btn-sm" id="clearAllList">Clear all</button>');
+        if ($(this).val() !== '') {
+            let newValue = $(this).val().replace(/ /g,'');
+            listItems.append("<option value='"+ newValue +"'>" + newValue + "</option>");
+            $(this).val("");
+        }
     }
 });
 
@@ -91,19 +76,26 @@ clearAllListBtn.on('click', function (e) {
     listItems.empty();
 });
 
-$.get("rest/getAllTables", function(array){
-    $.each( array, function( i, val ) {
-        tableSelect.append('<option value="'+ val['name'] +'">'+ val['name'] +'</option>')
+function getTables() {
+    $.get("rest/getAllTables", function (array) {
+        $.each(array, function (i, val) {
+            tableSelect.append('<option value="' + val['name'] + '">' + val['name'] + '</option>')
+        });
     });
-});
+}
 
-tableSelect.on('change', function(e) {
+function setAttributes() {
     $.get("rest/getAttributes?table="+tableSelect.val(), function(array){
         attributeSelect.empty();
-        attributeSelect.append('<option selected disabled>Choose an attribute</option>')
 
         $.each( array, function( i, val ) {
             attributeSelect.append('<option value="'+ val['name'] +'">'+ val['name'] +'</option>')
         });
     });
+}
+
+tableSelect.on('change', function(e) {
+    setAttributes();
 });
+
+getTables();
