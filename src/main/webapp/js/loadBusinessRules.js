36f2bafe3379c0603e058a7b6024ceca5408864a
Milo <milo.voorhout@student.hu.nl>
2020-01-27 14:53:19 +0100

    Nick can work with this

--- src/main/webapp/js/loadBusinessRules.js
+++ src/main/webapp/js/loadBusinessRules.js
@@ -3,16 +3,18 @@ function initPage() {
     $.get("generate/getBusinessRules/information", function (array) {
         $.each(array, function (i, val) {
             sessionStorage.setItem(val["id"], JSON.stringify(array[i]));
-            // console.log(array);
+            var valueText;
             if(i == 0) {
                 $('#list-tab').append('<a class="list-group-item list-group-item-action active" data-id="' + val["id"] + '" id="list-'+ val["id"] +'-list" data-toggle="list" href="#list-'+ val["id"] +'" role="tab" aria-controls="list-'+ val["id"] + '">' + val["name"] + '</a>');
-                getRuleData(val["id"], val["businessRuleTypeID"], val["subAttributeID"]);
-                loadCompareRuleData(array[i], val["id"], val["businessRuleTypeID"]);
+                valueText = getRuleData(val["id"], val["businessRuleTypeID"], val["subAttributeID"]);
+                loadCompareRuleData(array[i], val["id"], val["businessRuleTypeID"], valueText);
                 $('#list-' + val["id"]).addClass('active');
+
             } else {
                 $('#list-tab').append('<a class="list-group-item list-group-item-action" data-id="' + val["id"] + '" id="list-'+ val["id"] +'-list" data-toggle="list" href="#list-'+ val["id"] +'" role="tab" aria-controls="list-'+ val["id"] +'">' + val["name"] + '</a>');
-                getRuleData(val["id"], val["businessRuleTypeID"], val["subAttributeID"]);
-                loadCompareRuleData(array[i], val["id"], val["businessRuleTypeID"]);
+                valueText = getRuleData(val["id"], val["businessRuleTypeID"], val["subAttributeID"]);
+                loadCompareRuleData(array[i], val["id"], val["businessRuleTypeID"], valueText);
+                $('#list-' + val["id"]).addClass('active');
                 $('#list-'+ val["id"] +'').on('click', function (e) {
                     e.preventDefault()
                     $(this).tab('show')
@@ -54,6 +56,7 @@ function initPage() {
     // }
 
 function getRuleData(id, ruleType, subAttributeID) {
+    var valueInformation = "";
     switch (ruleType) {
         case 1:
             var minValue = 0;
@@ -66,9 +69,17 @@ function getRuleData(id, ruleType, subAttributeID) {
                         maxValue = val["value"];
                     }
                 });
-                // console.log(minValue, maxValue);
+                valueInformation =
+                    '    <tr>\n' +
+                    '       <th scope="row">Minimum value</th>\n' +
+                    '       <td> + minValue + </td>\n' +
+                    '    </tr>\n' +
+                    '    <tr>\n' +
+                    '       <th scope="row">Maximum value</th>\n' +
+                    '       <td>' + maxValue + '</td>\n' +
+                    '    </tr>\n';
+                return valueInformation;
             });
-            break;
         case 2:
             var subAttributeName = "";
             if(subAttributeID !== 0) {
@@ -76,7 +87,16 @@ function getRuleData(id, ruleType, subAttributeID) {
                     $.each(array, function (i, val) {
                         subAttributeName = val["tablename"] + "." + val["name"];
                     });
-                    console.log(subAttributeName);
+                    valueInformation =
+                    '    <tr>\n' +
+                    '       <th scope="row">Compare with</th>\n' +
+                    '       <td> Attribute </td>\n' +
+                    '    </tr>\n' +
+                    '    <tr>\n' +
+                    '       <th scope="row">Value</th>\n' +
+                    '       <td>' + subAttributeName + '</td>\n' +
+                    '    </tr>\n';
+                    return valueInformation;
                 });
             }
             var value = 0;
@@ -85,30 +105,49 @@ function getRuleData(id, ruleType, subAttributeID) {
                     $.each(array, function (i, val) {
                         value = val["value"];
                     });
-                    // console.log(value);
+                    valueInformation =
+                        '    <tr>\n' +
+                        '       <th scope="row">Compare with</th>\n' +
+                        '       <td> Literal value </td>\n' +
+                        '    </tr>\n' +
+                        '    <tr>\n' +
+                        '       <th scope="row">Value</th>\n' +
+                        '       <td>' + value + '</td>\n' +
+                        '    </tr>\n';
+                    return valueInformation;
                 });
             }
-
             break;
         case 3:
-
-            break;
+            var valueList = "[";
+            $.get("generate/getBusinessRules/values?id="+id, function (array) {
+                $.each(array, function (i, val) {
+                    var istLastElement = i == array.length -1;
+                    if (istLastElement) {
+                        if($.isNumeric(val["value"])) {
+                            valueList += val["value"] + "]";
+                        } else {
+                            valueList += "'" + val["value"] + "'" + "]";
+                        }
+                    } else {
+                        if($.isNumeric(val["value"])) {
+                            valueList += val["value"] + ", ";
+                        } else {
+                            valueList += "'" + val["value"] + "'" + ", ";
+                        }
+                    }
+                });
+                valueInformation =
+                    '    <tr>\n' +
+                    '       <th scope="row">Values</th>\n' +
+                    '       <td>' + valueList + '</td>\n' +
+                    '    </tr>\n';
+                return valueInformation;
+            });
     }
 }
 
-function getValues(id) {
-    var values = [];
-    
-    $.get("generate/getBusinessRules/values?id="+id, function (array) {
-        $.each(array, function (i, val) {
-            // values.push(val["type"] : val["value"]);
-            // values.push(val["type" ]: val["value"]);
-            // console.log(val["value"] + " " + val["type"]);
-        });
-    });
-}
-
-function loadCompareRuleData(array, id, information) {
+function loadCompareRuleData(array, id, information, valueText) {
     $('#nav-tabContent').append(
         '<div class="tab-pane fade show" id="list-'+ id +'" role="tabpanel" aria-labelledby="list-'+ id + '">\n' +
         '                                    <table class="table table-striped">\n' +
@@ -135,16 +174,7 @@ function loadCompareRuleData(array, id, information) {
         '                                            <th scope="row">tosad.productManagement.Operator</th>\n' +
         '                                            <td>' + information + '</td>\n' +
         '                                        </tr>\n' +
-        '                                        <!-- In case of Compare -->\n' +
-        '                                        <tr>\n' +
-        '                                            <th scope="row">Compare with</th>\n' +
-        '                                            <td>' + information + '</td>\n' +
-        '                                        </tr>\n' +
-        '                                        <!-- end -->\n' +
-        '                                        <tr>\n' +
-        '                                            <th scope="row">Value</th>\n' +
-        '                                            <td>' + information + '</td>\n' +
-        '                                        </tr>\n' +
+        '                                        ' + valueText + '\n' +
         '                                        <tr>\n' +
         '                                            <th scope="row">Validation Failure Severity</th>\n' +
         '                                            <td>' + information + '</td>\n' +
