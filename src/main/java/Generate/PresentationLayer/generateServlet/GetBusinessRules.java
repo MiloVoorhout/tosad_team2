23f22dedd8dd3549af5d17cba37261b2cb7cd98b
Stefan <stefan.jovanovic@live.nl>
2020-01-27 22:30:08 +0100

    Remaking the list page functionality

--- src/main/java/Generate/PresentationLayer/generateServlet/GetBusinessRules.java
+++ src/main/java/Generate/PresentationLayer/generateServlet/GetBusinessRules.java
@@ -13,6 +13,67 @@ import javax.ws.rs.core.MediaType;
 @Path("/getBusinessRules")
 public class GetBusinessRules {
 
+    @Path("/getMenuItems")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getMenuItems() throws SQLException {
+
+        JSONObject obj = new JSONObject();
+        JSONArray arr = new JSONArray();
+
+        Connection conn = DatabaseConnection.getInstance().getConnection();
+        String query  = "SELECT ID, NAME FROM TOSAD.BUSINESSRULE";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+
+        while (rs.next()) {
+            obj.put("id", rs.getInt("ID"));
+            obj.put("name", rs.getString("NAME"));
+            arr.put(obj);
+            obj = new JSONObject();
+        }
+
+        return (arr.toString());
+
+    }
+
+    @Path("/getContent")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getContent(@QueryParam("id") int id) throws SQLException {
+
+        JSONObject obj = new JSONObject();
+        JSONArray arr = new JSONArray();
+
+        Connection conn = DatabaseConnection.getInstance().getConnection();
+        String query  = "SELECT br.ID, br.name, br.FAILURETYPE, br.FAILUREMESSAGE, o.NAME as OPERATOR, " +
+                "A2.NAME AS ATTRIBUTE_NAME, A2.TABLENAME AS ATTRIBUTE_TABLE, B.NAME AS BUSINESSRULETYPE " +
+                "FROM TOSAD.BUSINESSRULE br " +
+                "INNER JOIN TOSAD.ATTRIBUTE A2 on br.ATTRIBUTEID = A2.ID " +
+                "INNER JOIN TOSAD.OPERATOR O on br.OPERATORID = O.ID " +
+                "INNER JOIN TOSAD.BUSINESSRULETYPE B on br.BUSINESSRULETYPEID = B.ID " +
+                "WHERE br.ID = "+id;
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+
+        while (rs.next()) {
+            obj.put("id", rs.getInt("ID"));
+            obj.put("operator", rs.getString("OPERATOR"));
+            obj.put("attributeName", rs.getString("ATTRIBUTE_NAME"));
+            obj.put("attributeTable", rs.getString("ATTRIBUTE_TABLE"));
+            obj.put("businessRuleType", rs.getString("BUSINESSRULETYPE"));
+            obj.put("name", rs.getString("NAME"));
+            obj.put("failureType", rs.getString("FAILURETYPE"));
+            obj.put("failureMessage", rs.getString("FAILUREMESSAGE"));
+            arr.put(obj);
+            obj = new JSONObject();
+        }
+
+        return (arr.toString());
+
+    }
+
+
     @Path("/information")
     @GET
     @Produces({MediaType.APPLICATION_JSON})
---
+++ src/main/webapp/js/test.js
@@ -0,0 +1,43 @@
+const listTab = $('#list-tab');
+const content = $('#nav-tabContent');
+
+const ruleType = $('#ruleType');
+const ruleTypeName = $('#ruleTypeName');
+const ruleTypeTable = $('#ruleTypeTable');
+const ruleTypeAttr = $('#ruleTypeAttr');
+const ruleTypeOperator = $('#ruleTypeOperator');
+const ruleTypeFailureType = $('#ruleTypeFailureType');
+const ruleTypeFailureMessage = $('#ruleTypeFailureMessage');
+let tempValue;
+
+function getContent(id){
+    $.get("generate/getBusinessRules/getContent?id="+id, function (array) {
+        $.each(array, function (i, val) {
+            ruleType.html(val['businessRuleType']);
+            ruleTypeName.html(val['name']);
+            ruleTypeAttr.html(val['attributeName']);
+            ruleTypeTable.html(val['attributeTable']);
+            ruleTypeOperator.html(val['operator']);
+            ruleTypeFailureType.html(val['failureType']);
+            ruleTypeFailureMessage.html(val['failureMessage']);
+        });
+    });
+}
+
+$.get("generate/getBusinessRules/getMenuItems", function (array) {
+    $.each(array, function (i, val) {
+        if (i === 0) {
+            listTab.append('<a class="list-group-item list-group-item-action listItem active" data-id="'+val['id']+'" id="list-'+val['id']+'-list" data-toggle="list" href="#list-'+val['id']+'" role="tab" aria-controls="list-'+val['id']+'" aria-selected="true">'+val['name']+'</a>');
+            getContent(val['id']);
+        }
+        else listTab.append('<a class="list-group-item list-group-item-action listItem" data-id="'+val['id']+'" id="list-'+val['id']+'-list" data-toggle="list" href="#list-'+val['id']+'" role="tab" aria-controls="list-'+val['id']+'">'+val['name']+'</a>');
+    });
+});
+
+$( "a" ).click(function() {
+    console.log("HALLO");
+
+    var id = $(this).attr("data-id");
+    alert(id);
+    getContent(id)
+});
